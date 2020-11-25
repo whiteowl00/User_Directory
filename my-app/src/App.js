8b529fccab12273c0e3501e3cb410e82822c4931
whiteowl00 <70918768+whiteowl00@users.noreply.github.com>
2020-11-24 20:16:53 -0500

    Updated final version

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Search'
+import MainContainer from './components/index'
+import './components/App.css'
+
+function App() {
+  
+
+  return (
+    <div className="body">
+      <Navbar />
+      <MainContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function Navbar(){
+    return (
+        <nav>
+            <h1>Random Employee Directory</h1>
+        </nav>
+    )
+}
+
+export default Navbar
